e0c04ad5e2fc56e33430c0ee6a6b5226c0be2c0c
Jonathan <atta.jon.t@gmail.com>
2019-10-30 11:56:07 +0100

    server added

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+app.get( "/index", function (req, res) {
+    console.log("access");
+    res.sendFile( __dirname + "/public/" + "index.html" );        
+});
+
+app.listen(port, function() {
+  console.clear();
+  console.log("app listening on port " + port);
+});
